b911553fdcfe5c48afb8d3fc2742ec97d10f2d62
Qin <aries.duanmu@gmail.com>
2017-12-05 23:33:11 +0800

    run and test, interesting^^

--- tetris.py
+++ tetris.py
@@ -31,20 +31,25 @@ class Board():
                 if piece.piece[y][x] == '#' and self.board[self.row_num + y - piece_height - level][offset + x] != '.':
                     return False
         return True
+    def try_place(self, piece, pos):
+        level, offset = pos
+        piece_height = len(piece)
+        board = [b[:] for b in self.board]
+        for y, line in enumerate(piece):
+            for x, block in enumerate(line):
+                if piece[y][x] != '.':
+                    board[self.row_num + y - piece_height - level][offset + x] = piece[y][x]
+        return len(list(filter(lambda x: "." not in x, board)))
 
     def place_piece(self, best_rot, best_pos):
-        print("--------")
-        print(best_rot)
-        print(best_pos)
-        print("--------")
+        
         level, offset = best_pos
         piece_height = len(best_rot)
         for y, line in enumerate(best_rot):
             for x, block in enumerate(line):
                 if best_rot[y][x] != '.':
                     self.board[self.row_num + y - piece_height - level][offset + x] = best_rot[y][x]
-        print('\n'.join(''.join(line) for line in self.board[-5:]))
-        print("--------")
+        
     
     def __str__(self):
         '''
@@ -88,38 +93,27 @@ class Board():
         return '\n'.join(''.join(line) for line in self.board)
 
 def find_best_score(board, piece):
-    best_score = None
-    best_rotation = None
-    best_position = None
-    
-    for level in range(len(board.board)):
-        
-        #if level > 0 and board.board[level - 1].count('#') == 0:
-            # Piece can't be placed here because it will fall down
-            #break
-        
-        for _ in range(4):
-            
-            for offset in range(len(board.board[0]) - len(piece.piece[0]) + 1):
+    results = []
+    for level in range(len(board.board)):   
+        for offset in range(len(board.board[0]) - len(piece.piece[0]) + 1):
+            for _ in range(4):
                 if board.piece_fits(piece,(level, offset)):
-
-                    # Surface area - level to drop
-                    # TODO better score caller
-                    score = piece.piece[0].count('#') - level
-                    
-                    if best_score is None or score > best_score:
-                        
-                        best_rotation = piece.piece
-                        best_position = (level, offset)
-                        best_score = score
-
-            piece.rotate()
-    return best_rotation, best_position     
+                    block_num = board.try_place(piece.piece, (level, offset))
+                    results.append([piece.piece, (level, offset), block_num])
+                piece.rotate()
+    
+    best = max(results, key = lambda x: x[2])
+    print("--------")
+    print("[*] best")
+    print(best[0], best[1])
+    print("--------")    
+    return best[0], best[1]
 
 def tetrisGame(pieces):
     board = Board()
     score = 0
     for p in pieces:
+        print("[*] p: {}".format(p))
         piece = Piece(p)
         best_rot, best_pos = find_best_score(board, piece)
         board.place_piece(best_rot, best_pos)
