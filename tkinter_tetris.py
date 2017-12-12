04f2f0ae393047077b600b16d713da1e3318e500
Ludisposed <pjgvanschaik@gmail.com>
2017-12-12 11:18:03 +0100

    Tkinter

--- tkinter_tetris.py
+++ tkinter_tetris.py
@@ -1,6 +1,40 @@
 from tkinter import Canvas, Label, Tk, StringVar
 from random import choice
 
+class Piece():
+    PIECES = (
+        ((0, 0), (1, 0), (0, 1), (1, 1)),     # square
+        ((0, 0), (1, 0), (2, 0), (3, 0)),     # line
+        ((2, 0), (0, 1), (1, 1), (2, 1)),     # right L
+        ((0, 0), (0, 1), (1, 1), (2, 1)),     # left L
+        ((0, 1), (1, 1), (1, 0), (2, 0)),     # right Z
+        ((0, 0), (1, 0), (1, 1), (2, 1)),     # left Z
+        ((1, 0), (0, 1), (1, 1), (2, 1)),     # T
+    )
+    
+    def __init__(self, piece):
+        self.piece = choice(Piece.PIECES)
+
+    @property
+    def width(self):
+        return len(self.piece[0])
+
+    @property
+    def height(self):
+        return len(self.piece)
+
+    def move(self):
+        pass
+
+    def hard_drop(self):
+        pass
+
+    def drop_line(self):
+        pass
+
+    def rotate(self, times=1):
+        for i in range(times % 4):
+            self.piece = [row[::-1] for row in zip(*self.piece)]
 
 class Tetris():
     WIDTH = 300
@@ -28,7 +62,6 @@ class Tetris():
         self.canvas.pack()
 
         self.root.bind("<Key>", self.handle_events)
-        self.timer()
         self.root.mainloop()
 
     def timer(self):
@@ -37,5 +70,8 @@ class Tetris():
     def handle_events(self, event):
         pass
 
-game = Tetris()
-    
+class Piece():
+    pass
+
+if __name__ == '__main__':
+    game = Tetris()    
