9444aded365f2efbd9fb1ed417a7e81be152d4d2
Ludisposed <pjgvanschaik@gmail.com>
2017-12-13 12:23:10 +0100

    update tkinter

--- Tetris2.0/tkinter_tetris.py
+++ Tetris2.0/tkinter_tetris.py
@@ -1,6 +1,9 @@
 from tkinter import Canvas, Label, Tk, StringVar
 from random import choice
 
+class Board():
+    pass
+
 class Tetris():
     WIDTH = 300
     HEIGHT = 500
@@ -24,23 +27,21 @@ class Tetris():
                              width = Tetris.WIDTH, 
                              height = Tetris.HEIGHT)
         
-        self.root.bind("<Key>", self.handle_events)
+        self.root.bind("<Key>", self.call_back)
         self.canvas.pack()
         self.root.mainloop()
 
-    def start_game(self):
-        pass
+    def start(self):
+        self.current_piece = Piece(self.canvas)
+        self.canvas.update()
 
-    def handle_events(self, event):
-        if repr(event.char) == "a":
-          #go left
-          pass
-        elif repr(event.char) == "d":
-          #go right
-          pass
-        elif repr(event.char) == "s":
-          #rotate
-          pass
+    def call_back(self, event):
+        if event.char in ["a", "\uf702"]:
+            pass
+        elif event.char in ["d", "\uf703"]:
+            pass
+        elif event.char in ["s", "\uf701"]:
+            self.current_piece.rotate()
 
 class Piece():
     BOX_SIZE = 20
@@ -56,16 +57,17 @@ class Piece():
     def __init__(self, canvas):
         self.piece = choice(Piece.PIECES)
         self.canvas = canvas
-        self.boxes = self.create_boxes
+        self.boxes = self.create_boxes()
 
     def create_boxes(self):
         boxes = []
-        for point in shape:
-            box = canvas.create_rectangle(point[0] * Piece.BOX_SIZE + Piece.START_POINT,
-                                          point[1] * Piece.BOX_SIZE,
-                                          point[0] * Piece.BOX_SIZE + Piece.BOX_SIZE + Piece.START_POINT,
-                                          point[1] * Piece.BOX_SIZE + Piece.BOX_SIZE,
-                                          fill=self.color)
+        for point in self.piece:
+            x, y = point
+            box = self.canvas.create_rectangle(x * Piece.BOX_SIZE + Piece.START_POINT,
+                                               y * Piece.BOX_SIZE,
+                                               x * Piece.BOX_SIZE + Piece.BOX_SIZE + Piece.START_POINT,
+                                               y * Piece.BOX_SIZE + Piece.BOX_SIZE,
+                                               fill="blue")
             boxes.append(box)
         return boxes
 
@@ -78,8 +80,46 @@ class Piece():
     def drop_line(self):
         pass
 
+    def can_move(self, position):
+        '''Check if box can move (x, y) boxes.'''
+        x, y = position
+        x = x * Piece.BOX_SIZE
+        y = y * Piece.BOX_SIZE
+        x_left, y_up, x_right, y_down = self.canvas.coords(box)
+
+        if y_down + y > Tetris.HEIGHT:
+            return False
+        if x_left + x < 0:
+            return False
+        if x_right + x > Tetris.WIDTH:
+            return False
+
+        return True
+
     def rotate(self):
-        self.piece = [row[::-1] for row in zip(*self.piece)]
+        boxes = self.boxes[:]
+        pivot = boxes.pop(2)
+
+        def pivot_box(box):
+            '''Return (x, y) boxes needed to rotate a box around the pivot.'''
+            box_coords = self.canvas.coords(box)
+            pivot_coords = self.canvas.coords(pivot)
+            x_diff = box_coords[0] - pivot_coords[0]
+            y_diff = box_coords[1] - pivot_coords[1]
+            x_pivot = (- x_diff - y_diff) / self.BOX_SIZE
+            y_pivot = (x_diff - y_diff) / self.BOX_SIZE
+            return (x_pivot, y_pivot)
+
+        if all(self.can_move(box, pivot_box(box)) for box in boxes):
+            for box in boxes:
+                x_move, y_move = get_move_coords(box)
+                self.canvas.move(box, 
+                                 x_move * self.BOX_SIZE, 
+                                 y_move * self.BOX_SIZE)
+
+        return True
+        
 
 if __name__ == '__main__':
-    game = Tetris()   
+    game = Tetris()
+    game.start()
