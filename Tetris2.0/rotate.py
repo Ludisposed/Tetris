5a278e479f0d9836e07481b5683ce2c342459f27
Qin <aries.duanmu@gmail.com>
2017-12-13 17:06:37 +0800

    update rotate

--- Tetris2.0/rotate.py
+++ Tetris2.0/rotate.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from random import randint, choice
 
+
 class Piece():
     def __init__(self, piece):
         self.piece = piece
@@ -21,6 +22,18 @@ class Piece():
     def random_rotate(self):
         self.rotate(randint(0,3))
     
+    #get coordinates of '#' which will used in draw on canvas
+    '''
+    such as
+
+        ##
+         ##
+
+        [('#','#','.'),('.','#','#')]
+    coordinates for # will be (0,0),(0,1),(1,1),(1,2)
+    but the right is          (0,0),(1,0),(1,1),(2,1)
+    SO we have to switch the `x` and `y`
+    '''
     def positions(self):
         return [(j,i) for i in range(self.height) for j in range(self.width) if self.piece[i][j] == "#"]
     
@@ -28,41 +41,56 @@ class Piece():
     def __str__(self):
        return '\n'.join(''.join(line) for line in self.piece)
 
-BLOCK_WIDTH = 10
-
 class Game:
-    def __init__(self, board):#board is canvas
-        self.pieces = self.all_pieces()
-        self.board = board
-        self.current_piece = None
 
-    def add_new_piece(self):
-        #when piece drop down set self.current_piece back to None 
-        if not self.current_piece:
-            self.current_piece = Piece(self.random_piece())
-        positions = self.current_piece.positions()
-        piece = []
+    def __init__(self):
+        root = Tk()
+
+        self.board = Canvas(root, width = 300, height = 500)
+        self.board.pack()
+
+        self.width, self.height = 50, 50
+
+        self.piece = Piece([('#','#','.'),('.','#','#')])
+        self.pieces = []
+        self.add_piece(0)
+
+        root.bind('<Key>', self.callback)
+        mainloop()
+
+    def add_piece(self, start_place):
+        positions = self.piece.positions()
+        self.pieces = []
         for pos in positions:
-            p = board.create_rectangle(pos[0] * BLOCK_WIDTH, \
-                pos[1] * BLOCK_WIDTH, BLOCK_WIDTH, BLOCK_WIDTH, fill="blue")
-            piece.append(p)
-        return piece
+          x,y = pos
+          self.pieces.append(self.board.create_rectangle(x * self.width + start_place, y * self.height + start_place, \
+                       (x + 1) * self.width + start_place, (y + 1) *  self.height + start_place, fill="blue"))
+       
 
-    def rotate(self):
-        self.current_piece.rotate()
-        #remove the canvas piece from canvas first
-        #self.add_new_piece()
-    
-    def random_piece(self):
-        return choice(self.pieces)
+    def remove_piece(self):
+        for piece in self.pieces:
+            self.board.delete(piece)
+        self.pieces = []
 
-    def all_pieces(self):
-        pieces = [[('.','#','#'),('#','#','.')],
-                  [('#','#','.'),('.','#','#')],
-                  [('#','#','#'),('.','.','#')],
-                  [('#','#','#'),('#','.','.')],
-                  [('#','#','#'),('.','#','.')],
-                  [('#','#'),('#','#')],
-                  [('#','#','#','#')]]
-        return pieces
+    def callback(self, event):
+
+        print(event.char)
+        if event.char in ["a", "\uf702"]:
+          print("go left")
+        elif event.char in ["d", "\uf703"]:
+          print("go right")
+        elif event.char in ["s", "\uf701"]:
+          print("rotate")
+          self.piece.rotate()
+          self.remove_piece()
+          self.add_piece(0)
+
+if __name__ == "__main__":
+    game = Game()
+
+
+
+
+
+
 
