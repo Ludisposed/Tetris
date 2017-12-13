d7974b8a85cd15e0edfdc3cba8fc741add4930bd
Qin <aries.duanmu@gmail.com>
2017-12-13 16:27:42 +0800

    move explaination

--- Tetris2.0/rotate.py
+++ Tetris2.0/rotate.py
@@ -1,40 +1,5 @@
 from tkinter import *
 from random import randint, choice
-def callback(event):
-    global start_x
-    print(event.char)
-    if event.char in ["a", "\uf702"]:
-      print("go left")
-      start_x -= 10
-      if start_x < 0:
-        start_x = 0
-    elif event.char in ["d", "\uf703"]:
-      print("go right")
-      start_x += 10
-      if start_x > 200:
-        start_x = 200
-    elif event.char in ["s", "\uf701"]:
-      print("rotate")
-    
-    board.coords(pieces[0], (start_x, start_y, start_x + 100, start_y + 50))
-    board.coords(pieces[1], (start_x + 50, start_y + 50, start_x + 150, start_y + 100))
-
-
-root = Tk()
-
-start_x, start_y = 0, 0
-
-board = Canvas(root, width = 300, height = 500)
-board.pack()
-
-
-piece1 = board.create_rectangle(0, 0, 100, 50, fill="blue")
-piece2 = board.create_rectangle(50, 50, 150, 100, fill="blue")
-
-pieces = [piece1, piece2]
-root.bind('<Key>', callback)
-mainloop()
-
 
 class Piece():
     def __init__(self, piece):
