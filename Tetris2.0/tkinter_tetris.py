f46315314f91bc88eb55a2cc2ce5fd9a3fd7632e
Ludisposed <pjgvanschaik@gmail.com>
2017-12-17 19:15:18 +0100

    Properties
    Seriously, you have done so much. I don't get all the changes you have done, But so far it does look good!
    
    I changed something to it having properties, but now maybe realise, these properties are never going to be used by other instances, and thus broke it for no reason xD? Anyway busy weekends, you can explain all tomorrow :)

--- Tetris2.0/tkinter_tetris.py
+++ Tetris2.0/tkinter_tetris.py
@@ -1,14 +1,22 @@
 from tkinter import Canvas, Label, Tk, StringVar, Button, LEFT
 from random import choice, randint
 
+
+"""
+I Althered the code a bit on som places, broke a few on others :p
+
+Other then that, great work on the refractor ^^
+"""
+
+# Idk if I like this bare Classes, Too Javaish?
 class Utility():
     PIECES = ([(0, 0), (1, 0), (0, 1), (1, 1)],     # Square
-          [(0, 0), (1, 0), (2, 0), (3, 0)],     # Line
-          [(2, 0), (0, 1), (1, 1), (2, 1)],     # Right L
-          [(0, 0), (0, 1), (1, 1), (2, 1)],     # Left L
-          [(0, 1), (1, 1), (1, 0), (2, 0)],     # Right Z
-          [(0, 0), (1, 0), (1, 1), (2, 1)],     # Left Z
-          [(1, 0), (0, 1), (1, 1), (2, 1)])     # T
+              [(0, 0), (1, 0), (2, 0), (3, 0)],     # Line
+              [(2, 0), (0, 1), (1, 1), (2, 1)],     # Right L
+              [(0, 0), (0, 1), (1, 1), (2, 1)],     # Left L
+              [(0, 1), (1, 1), (1, 0), (2, 0)],     # Right Z
+              [(0, 0), (1, 0), (1, 1), (2, 1)],     # Left Z
+              [(1, 0), (0, 1), (1, 1), (2, 1)])     # T
 
     BOX_SIZE = 20
 
@@ -16,8 +24,42 @@ class Utility():
     GAME_HEIGHT = 500
     GAME_START_POINT = GAME_WIDTH / 2 / BOX_SIZE * BOX_SIZE - BOX_SIZE
 
+
+class GameCanvas(Canvas):
+    def completed_lines(self, y_coords):
+        cleaned_lines = 0
+        all_boxes_coords = [(self.coords(box)[0], self.coords(box)[3])
+                            for box in self.find_all()
+                            if self.coords(box)[3] in y_coords]
+        
+        for y in y_coords:
+            if all( (x, y) in all_boxes_coords for x in range(10, Utility.GAME_WIDTH - 10, Utility.BOX_SIZE) ):
+                # Clear line
+                boxes_to_delete = [box
+                                   for box in self.find_all()
+                                   if self.coords(box)[3] == y]
+                for box in boxes_to_delete:
+                    self.delete(box)
+
+                # Drop boxes
+                boxes_to_drop = [box
+                                 for box in self.find_all()
+                                 if self.coords(box)[3] < y]
+                for box in boxes_to_drop:
+                    self.move(box, 0, Utility.BOX_SIZE)
+
+                cleaned_lines += 1
+        return cleaned_lines
+
+"""
+Do these properties also need setters?
+
+    @coords.setter
+    def coords(self, value):
+        self.__piece = value
+"""
+
 class Piece():
-    
     def __init__(self):
         self.__piece = choice(Utility.PIECES)
 
@@ -34,6 +76,7 @@ class Piece():
                        rotated[i][1] - self.__piece[i][1]) for i in range(len(self.__piece))]
 
         return directions
+
     def __rotate(self):
         max_x = max(self.__piece, key=lambda x:x[0])[0]
         new_original = (max_x, 0)
@@ -41,6 +84,8 @@ class Piece():
         return [(new_original[0] - coord[1],
                  new_original[1] + coord[0]) for coord in self.__piece]
 
+
+# Just a small, wtf does Square do and what does Box do? Maybe just don't get you here :p
 class Box():
     def __init__(self, canvas, coord, tag = -1):
         self.__canvas = canvas
@@ -72,7 +117,6 @@ class Box():
 
 class Square():
     def __init__(self, canvas, start_point, piece = None):
-        
         self.piece = piece
         if not piece:
             self.piece = Piece()
@@ -124,52 +168,52 @@ class Square():
            x_right + x > Utility.GAME_WIDTH or \
            overlap & other_items:
             return False
-        return True
-
-class GameCanvas(Canvas):
-
-    def completed_lines(self, y_coords):
-        cleaned_lines = 0
-        all_boxes_coords = [(self.coords(box)[0], self.coords(box)[3])
-                            for box in self.find_all()
-                            if self.coords(box)[3] in y_coords]
-        
-        for y in y_coords:
-            if all( (x, y) in all_boxes_coords for x in range(10, Utility.GAME_WIDTH - 10, Utility.BOX_SIZE) ):
-                # Clear line
-                boxes_to_delete = [box
-                                   for box in self.find_all()
-                                   if self.coords(box)[3] == y]
-                for box in boxes_to_delete:
-                    self.delete(box)
-
-                # Drop boxes
-                boxes_to_drop = [box
-                                 for box in self.find_all()
-                                 if self.coords(box)[3] < y]
-                for box in boxes_to_drop:
-                    self.move(box, 0, Utility.BOX_SIZE)
-
-                cleaned_lines += 1
-        return cleaned_lines
-
-        
+        return True        
 
 
+# I broke the Label with this, but this is more how properties work ^^
 class Tetris():
+    @property
+    def level(self):
+        return self._level
+
+    @level.setter
+    def level(self, level):
+        self._level = level
+
+    @property
+    def speed(self):
+        return self._speed
+
+    @speed.setter
+    def speed(self, speed):
+        self._speed = speed
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, score):
+        self._score = score
+
+    @property
+    def blockcount(self):
+        return self._blockcount
+
+    @blockcount.setter
+    def blockcount(self, blockcount):
+        self._blockcount = blockcount
 
     def __init__(self):
-        self._level = 1
-        self._score = 0
-        self._blockcount = 0
-
+        self.level = 1
+        self.score = 0
+        self.blockcount = 0
         self.speed = 500
-
         self.root = Tk()
         self.root.geometry("500x550") 
         self.root.title('Tetris')
         self.root.bind("<Key>", self.game_control)
-
         self.__game_canvas()
         self.__level_score_label()
         self.__next_square_canvas()
@@ -179,20 +223,26 @@ class Tetris():
             self.current_square.move((-1, 0))
         elif event.char in ["d", "D", "\uf703"]:
             self.current_square.move((1, 0))
-        elif event.char in ["s", "S", "\uf701"]:
-            self.hard_drop()
+        # Hard drop left for now, I wanna do it different...
+        # 1. Have a mirror piece, (What the piece will look like, if you do the "hard drop")
+        # 2. When "Hard Drop" is pressed, Fill the mirror piece and delete the old one. No more clunky speed things :)
+        # 
+        # elif event.char in ["s", "S", "\uf701"]:
+        #     self.hard_drop()
         elif event.char in ["w", "W", "\uf700"]:
             self.current_square.rotate()
 
-    def resume(self):
+    # Is this after a Pause? Or a new_game?
+    def new_game(self):
         self.score = 0
         self.blockcount = 0
+        self.level = 1
 
         self.canvas.delete("all")
         self.next_canvas.delete("all")
 
     def start(self):
-        self.resume()
+        self.new_game()
 
         self.current_square = Square(self.canvas, Utility.GAME_START_POINT)
         self.next_square = Square(self.next_canvas, 0)
@@ -210,21 +260,14 @@ class Tetris():
             self.next_canvas.delete("all")
             self.next_square = Square(self.next_canvas, 0)
 
-            #reset speed from hard drop
-            self.level = self.level
-
             if self.is_game_over():
                 return 
 
             self.blockcount += 1
                 
         self.root.after(self.speed, self.drop)
-    
-    def hard_drop(self):
-        self.speed = 10
 
-    def update_status(self):
-        
+    def update_status(self):        
         self.status_var.set(f"Level: {self.level}, Score: {self.score}")
         self.status.update()
 
@@ -234,7 +277,6 @@ class Tetris():
             self.quit_btn = Button(self.root, text="Quit", command=self.quit) 
             self.play_again_btn.place(x = Utility.GAME_WIDTH + 10, y = 200, width=100, height=25)
             self.quit_btn.place(x = Utility.GAME_WIDTH + 10, y = 300, width=100, height=25)
-
             return True
         return False
 
@@ -248,12 +290,10 @@ class Tetris():
 
     def completed_lines(self):
         y_coords = [box.coords[3] for box in self.current_square.boxes]
-        
         self.score += self.canvas.completed_lines(y_coords)
         
 
     def __game_canvas(self):
-        
         self.canvas = GameCanvas(self.root, 
                              width = Utility.GAME_WIDTH, 
                              height = Utility.GAME_HEIGHT)
@@ -272,35 +312,8 @@ class Tetris():
                                  height = 60)
         self.next_canvas.pack(padx=5 , pady=10)
     
-    #set & get
-    def __get_level(self):
-        return self._level
-
-    def __set_level(self, level):
-        self.speed = 500 - (level - 1) * 25
-        self._level = level
-        self.update_status()
-
-    def __get_score(self):
-        return self._score
-
-    def __set_score(self, score):
-        self._score = score
-        self.update_status()
-
-    def __get_blockcount(self):
-        return self._blockcount
-
-    def __set_blockcount(self, blockcount):
-        self.level = blockcount // 5 + 1
-        self._blockcount = blockcount
-
-    level = property(__get_level, __set_level)
-    score = property(__get_score, __set_score)
-    blockcount = property(__get_blockcount, __set_blockcount)
 
 
 if __name__ == '__main__':
     game = Tetris()
     game.start()
-
