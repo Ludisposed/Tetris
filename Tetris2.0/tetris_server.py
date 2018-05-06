0054bb124e9dda7442e23846cf85b16f703f37c1
Qin <aries.duanmu@gmail.com>
2018-05-06 22:17:27 +0800

    add log

--- Tetris2.0/tetris_server.py
+++ Tetris2.0/tetris_server.py
@@ -1,25 +1,26 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Action => Consequence
 MSG = {b"Line Finished": b"Add Line", 
        b"Game Over": b"Game Over"}
 
 import socket
+import logging
 
 class TetrisServer:
     def __init__(self, ip="localhost", port=9090):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((ip, port))
-        print(f"[*] Server is listening on {ip}:{port}")
+        logging.info(f"[*] Server is listening on {ip}:{port}")
         self.server.listen(2)
         self.clients = self.get_clients()
 
     def get_clients(self):
-        print("[*] Waiting for players....")
+        logging.info("[*] Waiting for players....")
         clients, client_id = {}, 0
         while client_id < 2:
             conn, addr = self.server.accept()
-            print(f"[*] Connection from {addr}, CleintID {client_id}")
+            logging.info(f"[*] Connection from {addr}, CleintID {client_id}")
             clients[client_id] = conn
             client_id += 1
         return clients
@@ -31,23 +32,40 @@ class TetrisServer:
                 for client_id in self.clients.keys():
                     msg = self.clients[client_id].recv(1024)
                     if msg:
-                        print(f"[*] Recieved {msg}")
+                        logging.info(f"[*] Recieved {msg}")
                         client_id = int(not client_id)
                         self.clients[client_id].send(MSG[msg])
-                        print(f"[*] Send to other client {MSG[msg]}")
+                        logging.info(f"[*] Send to other client {MSG[msg]}")
                         game_over = msg == b"Game Over"
         except KeyboardInterrupt:
-            print("[!] Exiting")
+            logging.info("[!] Exiting")
         except Exception as error:
-            print(f"[!] Debugging Error\n[!] {error}")
+            logging.error(f"[!] Debugging Error\n[!] {error}")
         finally:
             self.close()
 
     def close(self):
-        self.server.shutdown(socket.SHUT_RDWR)
-        self.server.close()
-        print("[*] Server has shutdown")
+        try:
+            self.server.shutdown(socket.SHUT_RDWR)
+            self.server.close()
+            logging.info("[*] Server has shutdown")
+        except Exception as error:
+            logging.error(f"[!] Debugging Error\n[!] {error}")
+
+def logconfig():
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                        datefmt='%a, %d %b %Y %H:%M:%S',
+                        filename='tetris_server.log',
+                        filemode='a+')
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
 
 if __name__ == "__main__":
+    logging.getLogger(__name__)
+    logconfig()
     t_server = TetrisServer()
     t_server.start_game()
