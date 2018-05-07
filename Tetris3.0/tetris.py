2337aaa910788f1342063b5cd10cf85dd3534feb
root <root@localhost.localdomain>
2018-05-07 12:19:11 -0400

    Send from directory

--- Tetris3.0/tetris.py
+++ Tetris3.0/tetris.py
@@ -1,39 +1,12 @@
 #!/usr/bin/python3
 
 from flask import Flask, request, send_from_directory
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
 
 @app.route('/')
-def root():
-    return """<!DOCTYPE html>
-<html>
-<head>
-    <title>Tetris</title>
-    <style>
-        body {
-            background-color: #272821;
-        }
-        .text {
-            color: #706C5A;
-            font-family: Inconsolata, Courier, monospace;
-            font-size: 20px;
-        }
-        #score {
-            padding-left: 55%;
-        }
-        #output {
-            float: left;
-            padding-left: 20%;
-        }
-    </style>
-</head>
-<body>
-    <div id="output" class="text"></div>
-    <div id="score" class="text"></div>
-    <script src=./js/tetris.js></script>
-    <script type="text/javascript"></script>
-</body>
-</html>"""
+@app.route('/index')
+def index():
+    return send_from_directory('', "tetris.html")
 
 @app.route('/js/<path:path>')
 def send_js(path):
