e3763e02ddfe164d82f7a77d4953c485ef4759ee
ariesduanmu <aries.duanmu@gmail.com>
2018-05-28 23:54:20 +0800

    add simple tetris agent and model

--- gym_tetris/QSimpleTetris.py
+++ gym_tetris/QSimpleTetris.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python
 import numpy as np
+from random import randint
+
 class QModel(object):
+    def __init__(self, Q):
+        self.Q = Q
+
+    def predict(self, state):
+        piece = bin(state)[2:-12]
+        state = bin(state)[-12:]
+        idx = (int(piece[:-6],2) * 3 + int(piece[-2:], 2)) * int(state, 2)
+        return self.Q[idx]
+
+    def update(self, state_memories, move_memories, reward_memories):
+        #update the model
+        #choose the best reword for each state update Q
+        pass
+
+class QAgent(object):
     def __init__(self, model_path = "simple_tetris_Q"):
         self.width = 6
         self.alpha = 0.1
@@ -9,25 +26,49 @@ class QModel(object):
         self.model_path = model_path
         self.read_dataset()
 
-    def predict(self, state):
-        piece = bin(state)[2:-12]
-        state = bin(state)[-12:]
-        idx = (int(piece[:-6],2) * 3 + int(piece[-2:], 2)) * int(state, 2)
-        return self.Q[idx]
+    def buildmodel(self):
+        Q = self.read_dataset()
+        self.model = QModel(Q)
 
-    def train(self):
-        pass
+    def next_move(self, state, render=False):
+        mov = self.model.predict(state)
+        return mov
 
+    def update_model(self, state_memories, move_memories, reward_memories):
+        self.model.update(state_memories, move_memories, reward_memories)
+        self.save_dataset(self.model.Q)
 
-    def save_dataset(self):
-        with open(self.model_path, 'wb+') as f:
-            pickle.dump((self.Q, self.archive), f, -1)
+    def save_dataset(self, Q):
+        fd = open(self.model_path, 'wb+')
+        pickle.dump((Q), fd, -1)
+        fd.close()
             
 
     def read_dataset(self):
         if not os.path.isfile(self.model_path):
-            self.Q = [0 for _ in range((1 << (2 * self.width)) * 12)]
-            self.archive = []
+            Q = [0 for _ in range((1 << (2 * self.width)) * 12)], []
         else:
-            with open(self.model_path, 'rb') as f:
-                self.Q, self.archive = pickle.load(f)
+            fd = open(self.model_path, 'rb')
+            Q = pickle.load(fd)
+            fd.close()
+        return Q
+
+class QTrain(object):
+    def __init__(self, episodes = 10000, epsilon = 0.2):
+        self.epsilon = epsilon
+        self.episodes = episodes
+        self.agent = QAgent()
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return randint(0,23)
+        else:
+            return self.agent.next_move(state)
+
+    def train(self):
+        for i in range(self.episodes):
+            #train env
+            pass
+
+
+
